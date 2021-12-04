8d5246b3c862dff2c5b6d5a70c3d45304ee771c6
Vid Drobnič <vid.drobnic@gmail.com>
2021-12-04 17:08:31 +0100

    new year, new aoc 🎅

---
+++ 2021/days/day01/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let input = include_str!("input.txt");
+    let input: Vec<i32> = input
+        .split_ascii_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    day_01(&input);
+    day_02(&input);
+}
+
+fn day_01(input: &Vec<i32>) {
+    let (res, _) = input
+        .into_iter()
+        .fold((0, None), |(res, prev), x| match prev {
+            None => (res, Some(x)),
+            Some(y) if x > y => (res + 1, Some(x)),
+            _ => (res, Some(x)),
+        });
+
+    println!("Part one: {}", res);
+}
+
+fn day_02(input: &Vec<i32>) {
+    let mut res = 0;
+    for i in 1..(input.len() - 2) {
+        let prev = input[i - 1] + input[i] + input[i + 1];
+        let current = input[i] + input[i + 1] + input[i + 2];
+        if current > prev {
+            res += 1;
+        }
+    }
+
+    println!("Part two: {}", res);
+}
